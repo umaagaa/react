9d52aec17e22bedc745a8e85532fe08a69f198b6
Sejfudin <sejfudin.duranovic@gmail.com>
2024-11-14 23:42:02 +0100

    fix:Add no image poster for each movie that does not have own image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Card = ({ movie }) => {
+
+    const { title, vote_average, poster_path } = movie; //destructuring
+
+    // Make sure to have a default image in case the movie doesn't have a poster
+    const image = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNNLEL-qmmLeFR1nxJuepFOgPYfnwHR56vcw&s';
     return (
         <div className="bg-white p-4 rounded shadow">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full h-auto mb-2 rounded" />
-            <h2 className="text-lg font-semibold">{movie.title}</h2>
-            <p>Rating: {movie.vote_average}</p>
+            <img src={image} alt={title} className="min-h-[308px] mb-2 rounded" />
+            <h2 className="text-lg font-semibold">{title}</h2>
+            <p>Rating: {vote_average}</p>
         </div>
     );
 };
