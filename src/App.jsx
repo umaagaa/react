cc7a8a5697b9574ce922e9d549ccbc51ca71ccfd
umaagaa <umejr.mujagic@hotmail.com>
2024-11-17 22:16:25 +0100

    on click event

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import movies from './helpers/movies'; 
+import React, { useState } from 'react';
+import moviesData from './helpers/movies'; // Pravilno importujemo moviesData iz movies.js
 import Card from './components/Card'; 
 
 const App = () => {
+  const [movies, setMovies] = useState(moviesData); // Inicijaliziramo movies sa moviesData
+
+  const handleDelete = (id) => {
+    const updatedMovies = movies.filter((movie) => movie.id !== id);
+    setMovies(updatedMovies);
+  };
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Movies Gallery</h1>
-      <div className="grid grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
         {movies.map((movie) => (
-          <Card key={movie.id} movie={movie} />
+          <Card key={movie.id} movie={movie} onDelete={() => handleDelete(movie.id)} />
         ))}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
-const Card = ({ movie }) => {
+const Card = ({ movie, onDelete }) => {
+    const { title, vote_average, poster_path } = movie;
 
-    const { title, vote_average, poster_path } = movie; //destructuring
-
-    // Make sure to have a default image in case the movie doesn't have a poster
     const image = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNNLEL-qmmLeFR1nxJuepFOgPYfnwHR56vcw&s';
+
     return (
-        <div className="bg-white p-4 rounded shadow">
+        <div onClick={onDelete} className="bg-white p-4 rounded shadow cursor-pointer">
             <img src={image} alt={title} className="min-h-[308px] mb-2 rounded" />
             <h2 className="text-lg font-semibold">{title}</h2>
             <p>Rating: {vote_average}</p>
-        </div>
+         </div>
     );
 };
 
